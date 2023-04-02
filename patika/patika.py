17164abfd1ac8914731d4933c25479fb4ca21bf3
mmeerrkkeell <127442610+mmeerrkkeell@users.noreply.github.com>
2023-04-03 00:10:53 +0300

    www.patika.dev-3.proje
    algoritma-3.proje

---
+++ patika/patika.py
@@ -0,0 +1,19 @@
+Proje 3
+[7, 5, 1, 8, 3, 6, 0, 9, 4, 2] dizisinin Binary-Search-Tree aşamalarını yazınız.
+
+Örnek: root x'dir. root'un sağından y bulunur. Solunda z bulunur vb.
+
+
+root=6 olarak alalım
+
+                         6 
+                     /       \
+                    /         \
+                   5           7
+                  / \         / \ 
+                 /   \       /   \
+                1     8     3     9
+               / \         /
+              /   \       /    
+             0     4     2
+       
